4af890c29258a326951a56643b796db000e939fd
sohag <sohagaiub64@gmail.com>
2019-10-26 01:36:24 +0600

    make ready some tables for job portal

---
+++ database/migrations/2019_10_25_185354_create_jobs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->string('position');
+            $table->text('address');
+            $table->string('type');
+            $table->string('status');
+            $table->date('last_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
---
+++ database/migrations/2019_10_25_185442_create_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->text('address');
+            $table->string('gender');
+            $table->date('dob');
+            $table->text('experience');
+            $table->text('bio');
+            $table->text('cover_letter');
+            $table->string('resume');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
